e540778277afe97b76f70389ab59b7ddae96fa9d
JDAAR <jdanaconar@gmail.com>
2025-02-14 14:21:27 -0500

    Frontend : se realizo un modal generico utilizando redux para manejar su estado y enviado contenido por medio de las props, se estilizo y consumio la api de crear una nueva dependencia al cliente seleccionado

--- Backend_playSystem/src/main/java/org/api/java/Backend_playSystem/entities/DependenciaEntity.java
+++ Backend_playSystem/src/main/java/org/api/java/Backend_playSystem/entities/DependenciaEntity.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class DependenciaEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private String idDependencia;
 
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
